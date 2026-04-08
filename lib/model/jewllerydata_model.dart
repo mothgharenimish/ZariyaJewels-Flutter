bc582153405ec7856f8fa7a1c1afcbf0fb72ecfd
mothgharenimish <mothgharenimish0@gmail.com>
2026-04-08 18:02:28 +0530

    Third Commit

---
+++ lib/model/jewllerydata_model.dart
@@ -0,0 +1,73 @@
+import 'dart:convert';
+
+Welcome welcomeFromJson(String str) => Welcome.fromJson(json.decode(str));
+
+String welcomeToJson(Welcome data) => json.encode(data.toJson());
+
+class Welcome {
+  bool status;
+  String message;
+  List<Datum> data;
+
+  Welcome({
+    required this.status,
+    required this.message,
+    required this.data,
+  });
+
+  factory Welcome.fromJson(Map<String, dynamic> json) => Welcome(
+    status: json["status"],
+    message: json["message"],
+    data: List<Datum>.from(json["data"].map((x) => Datum.fromJson(x))),
+  );
+
+  Map<String, dynamic> toJson() => {
+    "status": status,
+    "message": message,
+    "data": List<dynamic>.from(data.map((x) => x.toJson())),
+  };
+}
+
+class Datum {
+  int id;
+  String jewlleryname;
+  String jewlleryprice;
+  String jewllerydescription;
+  String jewllerycategory;
+  String jewllcategoryId;
+  String jewllavailableItemquantity;
+  String jewllImage;
+
+  Datum({
+    required this.id,
+    required this.jewlleryname,
+    required this.jewlleryprice,
+    required this.jewllerydescription,
+    required this.jewllerycategory,
+    required this.jewllcategoryId,
+    required this.jewllavailableItemquantity,
+    required this.jewllImage,
+  });
+
+  factory Datum.fromJson(Map<String, dynamic> json) => Datum(
+    id: json["id"],
+    jewlleryname: json["jewlleryname"],
+    jewlleryprice: json["jewlleryprice"],
+    jewllerydescription: json["jewllerydescription"],
+    jewllerycategory: json["jewllerycategory"],
+    jewllcategoryId: json["jewllcategory_id"],
+    jewllavailableItemquantity: json["jewllavailable_itemquantity"],
+    jewllImage: json["jewll_image"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "id": id,
+    "jewlleryname": jewlleryname,
+    "jewlleryprice": jewlleryprice,
+    "jewllerydescription": jewllerydescription,
+    "jewllerycategory": jewllerycategory,
+    "jewllcategory_id": jewllcategoryId,
+    "jewllavailable_itemquantity": jewllavailableItemquantity,
+    "jewll_image": jewllImage,
+  };
+}
--- lib/screen/Home/home.dart
+++ lib/screen/Home/home.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -8,32 +9,183 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final TextEditingController _searchController = TextEditingController();
+
+
+  final dio = Dio();
+
+
+  @override
+  void initState() {
+    super.initState();
+
+    getHttp();
+
+  }
+
+  void getHttp() async {
+    final response = await dio.get('https://jewellerydata.infinityfreeapp.com/jewlleryread.php');
+    print("The response is $response");
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            padding: const EdgeInsets.all(10),
-            child: Row(
-              children: [
-                Image.asset(
-                  "assets/images/Frame 6.png",
-                  height: 45,
-                  width: 45,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(right: 0,left: 0),
+              child: Container(
+                padding: const EdgeInsets.only(left: 16),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.white.withValues(alpha: 0.08),
+                      blurRadius: 8,
+                      spreadRadius: 1,
+                      offset: const Offset(0, 4),
+                    ),
+                  ],
                 ),
-                const SizedBox(width: 10), // space between image and text
-                const Text(
-                  "Hello Nimish",
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
+                child: Row(
+                  children: [
+                    Image.asset(
+                      "assets/images/Frame 6.png",
+                      height: 45,
+                      width: 45,
+                    ),
+                    const SizedBox(width: 5),
+                    const Text(
+                      "ZARIYA JEWELS",
+                      style: TextStyle(
+                        fontFamily: "Lora",
+                        fontSize: 17,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+
+                    const Spacer(),
+
+                    Container(
+                      padding: const EdgeInsets.only(right:  16),
+                      child: Row(
+                        children: [
+                            CircleAvatar(
+                              radius: 18,
+                              backgroundColor: const Color(0xFF000000).withValues(alpha: 0.10),
+                              child: Image.asset(
+                                "assets/images/material-symbols-light_favorite-outline.png",
+                                height: 26 ,
+                                width: 26,
+                              ),
+                            ),
+                          SizedBox(width: 4),
+                          CircleAvatar(
+                            radius: 18,
+                            backgroundColor: const Color(0xFF000000).withValues(alpha: 0.10),
+                            child: Image.asset(
+                              "assets/images/mingcute_notification-line.png",
+                              height: 26 ,
+                              width: 26,
+                            ),
+                          ),
+                          SizedBox(width: 4),
+                          CircleAvatar(
+                            radius: 18,
+                            backgroundColor: const Color(0xFF000000).withValues(alpha: 0.10),
+                            child: Image.asset(
+                              "assets/images/iconamoon_scanner-light.png",
+                              height: 26 ,
+                              width: 26,
+                            ),
+                          ),
+                        ],
+                      ) ,
+                    )
+                  ],
+                ),
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.only(top: 38,left: 16,right: 40),
+              child: RichText(
+                text: TextSpan(
+                  style: const TextStyle(
+                    fontSize: 32,
+                    fontFamily: "Lora",
+                    fontWeight: FontWeight.w500,
+                  ),
+                  children: [
+                    TextSpan(
+                      text: "The Art of ",
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                    ),
+                    TextSpan(
+                      text: "Exceptional Jewelry",
+                      style: TextStyle(
+                        color: Colors.brown,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.only(top: 19,left: 16,right: 16),
+              child: Container(
+                height: 50,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.black.withValues(alpha: 0.20),
+                    width: 2,
+                  ),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 8,right: 8),
+                  child: Row(
+                    children: [
+                      Image.asset("assets/images/line-md_search.png",
+                        height: 24,
+                        width: 24,
+                      ),
+                      const SizedBox(width: 8),
+                       Expanded(
+                         child: TextFormField(
+                           controller: _searchController,
+                           keyboardType: TextInputType.name,
+                           decoration: InputDecoration(
+                             hintText: "Search Jewllery Products",
+                             hintStyle: TextStyle(color: Colors.grey),
+                             border: InputBorder.none,
+                             contentPadding: EdgeInsets.symmetric(vertical: 14),
+                           ),
+                         ),
+                       ),
+
+                      Image.asset("assets/images/icon-park-outline_voice.png",
+                        height: 24,
+                        width: 24,
+                      ),
+
+                    ],
                   ),
                 ),
-              ],
-            ),
-          ),
-        ],
+
+              ),
+            )
+
+          ],
+        ),
       ),
     );
   }
