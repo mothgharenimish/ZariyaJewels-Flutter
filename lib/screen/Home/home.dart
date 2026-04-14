7a8df86205e97191f43de71dfaf037870051c02c
mothgharenimish <mothgharenimish0@gmail.com>
2026-04-14 12:20:47 +0530

    Eight Commit

--- lib/screen/Home/home.dart
+++ lib/screen/Home/home.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:image/image.dart' as img;
+import 'package:image_picker/image_picker.dart';
+import 'package:tflite_flutter/tflite_flutter.dart';
 import 'package:zariyajewllery/model/jewllerydata_model.dart';
 import 'package:zariyajewllery/screen/Home/categorycard.dart';
 import 'package:zariyajewllery/screen/Home/customalert.dart';
@@ -14,9 +18,23 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   final TextEditingController _searchController = TextEditingController();
-
-
   final dio = Dio();
+  File? image;
+  Interpreter? interpreter;
+  bool modelloaded = false;
+  String result = "No Prediction";
+  var dismissed = false;
+  List<Datum> finalList = [];
+
+ List<String> classes = [
+  "Bangles",
+   "bracelet",
+   "cushiondiamond",
+   "Earings",
+   "heartdiamond",
+   "necklace",
+   "ring"
+];
 
 
   @override
@@ -24,7 +42,54 @@ class _HomeState extends State<Home> {
     super.initState();
 
     getHttp();
+    loadmodel();
+  }
 
+Future loadmodel() async {
+
+    try {
+
+    interpreter = await Interpreter.fromAsset("assets/nasnet_image_classifiers.tflite");
+
+    print("The interpreter is $interpreter");
+    print("Model loaded successfully");
+
+    setState(() {
+      modelloaded = true;
+    });
+
+    } catch(error) {
+        print("The error is there $error");
+    }
+}
+
+  Future getImage(ImageSource source) async {
+
+    if (!modelloaded) {
+      print("Model still loading...");
+      return;
+    }
+
+    if (Navigator.canPop(context)) {
+      Navigator.pop(context);
+    }
+
+    final pickedImage = await ImagePicker().pickImage(source: source);
+
+    if (pickedImage == null) return;
+
+    final selectedImage = File(pickedImage.path);
+
+
+    setState(() {
+      image = selectedImage;
+      print("The image is $image");
+      // predictImage(selectedImage);
+
+    });
+
+  var b =   predictImage(selectedImage);
+  print("The b is $b");
   }
 
   List<Datum> jewelleryList = [];
@@ -34,7 +99,7 @@ class _HomeState extends State<Home> {
   Future<void> getHttp() async {
     try {
       final response = await dio.get(
-        'http://192.168.1.6:8080/myproject/JewelleryPAPI/fetch_jewellery.php',
+        'http://192.168.1.5:8080/myproject/JewelleryPAPI/fetch_jewellery.php',
       );
 
       print(response);
@@ -78,6 +143,76 @@ class _HomeState extends State<Home> {
   }
 
 
+  Future predictImage(File file) async {
+
+    if (interpreter == null) {
+      print("Model not loaded yet");
+      return;
+    }
+
+    final imageBytes = await file.readAsBytes();
+
+    img.Image? originalImage = img.decodeImage(imageBytes);
+
+    if (originalImage == null) {
+      print("Image decode failed");
+      return;
+    }
+
+    img.Image resizedImage =
+    img.copyResize(originalImage, width: 224, height: 224);
+
+    var input = List.generate(
+        1,
+            (index) => List.generate(
+            224,
+                (y) => List.generate(
+                224,
+                    (x) => List.generate(
+                    3,
+                        (c) => resizedImage.getPixel(x, y)[c] / 255.0))));
+
+    var output = List.generate(1, (index) => List.filled(classes.length, 0.0));
+
+    interpreter!.run(input, output);
+
+    int predictedIndex = 0;
+    double confidence = 0;
+
+    for (int i = 0; i < classes.length; i++) {
+      if (output[0][i] > confidence) {
+        confidence = output[0][i];
+        predictedIndex = i;
+      }
+    }
+
+    String predictedClass = classes[predictedIndex];
+    
+    print("The Predication class is $predictedClass");
+
+    setState(() {
+      result =
+      "$predictedClass \nConfidence: ${(confidence * 100).toStringAsFixed(2)}%";
+
+      print("The result is $result");
+    });
+    
+    findSimilarProducts(predictedClass);
+  }
+
+  void findSimilarProducts(String predictedClass) {
+    for (var item in jewelleryList) {
+      if (item.jewllerycategory.trim().toLowerCase() ==
+          predictedClass.toLowerCase()) {
+        print("Name: ${item.jewlleryname}");
+        print("Category: ${item.jewllerycategory}");
+        print("Price: ${item.jewlleryprice}");
+        print("Image: ${item.jewllImage}");
+      }
+    }
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -87,7 +222,7 @@ class _HomeState extends State<Home> {
         : jewelleryList.where((item) =>
     item.jewllerycategory.trim() == selectedCategory).toList();
 
-    List<Datum> finalList = searchcode(filteredList);
+     finalList = searchcode(filteredList);
 
     return Scaffold(
       body: SafeArea(
@@ -161,7 +296,7 @@ class _HomeState extends State<Home> {
                                     shape: RoundedRectangleBorder(
                                       borderRadius: BorderRadius.circular(20),
                                     ),
-                                    child: Customalert(onGalleryClick: onGalleryClick, onCameraClick: onCameraClick),
+                                    child: Customalert(onGalleryClick: () => getImage(ImageSource.gallery), onCameraClick: () => getImage(ImageSource.camera)),
 
                                   );
                                 },
